2937df99780a518331f69a45008f9bd5d0644c70
kadraknb <kadrak02@hotmail.com>
2023-08-30 03:14:34 -0300

    add fun para verificar se e uma page de drinks.

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -4,8 +4,13 @@ import AppContext from './AppContext';
 
 function AppProvider({ children }) {
   const [recipesUpdate, setRecipesUpdate] = useState([]);
+  
+  const [isDrinkPage, setDrinkPage] = useState(false);
+  const setIsDrinkPage = (page) => {
+    setDrinkPage(page === 'Drinks')
+  }
 
-  const value = { recipesUpdate, setRecipesUpdate };
+  const value = { recipesUpdate, setRecipesUpdate, isDrinkPage, setIsDrinkPage };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 }
