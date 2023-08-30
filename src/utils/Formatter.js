904fdaec72244a6d61f95a7b713e876f63021bb8
kadraknb <kadrak02@hotmail.com>
2023-08-30 03:17:28 -0300

    criando um class para formata obj.

---
+++ src/utils/Formatter.js
@@ -0,0 +1,36 @@
+export default class Formatter {
+  static recipeDetail = (r, boo) => {
+    return {
+      id: boo ? r.idDrink : r.idMeal,
+      name: boo ? r.strDrink : r.strMeal,
+      thumb: boo ? r.strDrinkThumb : r.strMealThumb,
+      category: boo ? r.strAlcoholic : r.strCategory,
+      instructions: r.strInstructions,
+      youtube: boo ? '' : r.strYoutube.replace('watch?v=', 'embed/'),
+      ingredient: this.__transformArray(r, 'strIngredient'),
+      measure: this.__transformArray(r, 'strMeasure'),
+    };
+  };
+
+  static recipeCard = (r, boo) => {
+    return {
+      id: boo ? r.idDrink : r.idMeal,
+      name: boo ? r.strDrink : r.strMeal,
+      thumb: boo ? r.strDrinkThumb : r.strMealThumb,
+    };
+  };
+
+  static recipeRecommend = (r, boo) => (
+    r.map((obj) => ({
+      id: boo ? obj.idDrink : obj.idMeal,
+      name: boo ? obj.strDrink : obj.strMeal,
+      thumb: boo ? obj.strDrinkThumb : obj.strMealThumb,
+    }))
+  );
+
+  static __transformArray = (r, str) => {
+    return Object.entries(r)
+      .filter(([k, v]) => k.includes(str) && v === null)
+      .map(([_, v]) => v);
+  };
+}
