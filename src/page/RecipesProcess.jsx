1a174786a030b95ae919171297ec4c7cb1d0fb9c
kadraknb <kadrak02@hotmail.com>
2023-08-31 03:32:07 -0300

    criando page de processo de receita.

---
+++ src/page/RecipesProcess.jsx
@@ -0,0 +1,49 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import GetApi from '../services/getApi';
+import { useRoute } from '@react-navigation/native';
+import AppContext from '../context/AppContext';
+import CardDetail from '../components/CardDetail';
+import GreeButton from '../components/Buttons/GreeButton';
+
+const RecipesProcess = () => {
+  const [recipe, setRecipe] = useState({});
+  const [loading, setLoading] = useState(false);
+  const {isDrinkPage} = useContext(AppContext);
+
+  const { recipeId } = useRoute().params;
+
+  useEffect(() => {
+    getRecipe();
+    return () => {};
+  }, []);
+
+  const getRecipe = async () => {
+    const data = await GetApi.recipesById(isDrinkPage, recipeId);
+    setRecipe(data);
+    setLoading(true);
+  };
+  
+  return (
+    <View>
+      {loading && (
+        <View>
+          <CardDetail recipe={recipe} />
+          <View>
+          <Text>Ingredients</Text>
+            {recipe.ingredient.map(( ingredient, i) => (
+              <Text key={ingredient + new Date().getTime()}>
+                {`${ingredient} ${recipe.measure[i]}`}
+              </Text>
+            ))}
+          </View>
+          <Text>Instructions</Text>
+          <Text>{recipe.instructions}</Text>
+          <GreeButton title="Finish Recipe" newRota="RecipesProcess" />
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default RecipesProcess;
